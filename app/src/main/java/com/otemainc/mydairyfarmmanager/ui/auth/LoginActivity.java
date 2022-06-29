cbb67a1d629a738b4d85109333a2caf6752b908f
Theonlysmartboy <o2jose43@gmail.com>
2022-06-29 17:21:21 +0300

    Begun adding welcome module

---
+++ app/src/main/java/com/otemainc/mydairyfarmmanager/ui/auth/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.otemainc.mydairyfarmmanager.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.otemainc.mydairyfarmmanager.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
